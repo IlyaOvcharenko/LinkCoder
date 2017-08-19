0a19bd540a0131cb33608ad7584e0a1a5ea89fe0
IlyaOvcharenko <ibovcharenko@gmail.com>
2017-08-19 04:02:52 +0300

    Pagination

--- Source/LinkCoder/Web/Scripts/app/Controllers/historyCtrl.js
+++ Source/LinkCoder/Web/Scripts/app/Controllers/historyCtrl.js
@@ -4,21 +4,37 @@
         $rootScope.currentTab = 'history';
         $scope.baseUrl = $location.protocol() + '://' + $location.host() + ':' + $location.port() + '/';
 
-        $scope.search = function () {
-            $scope.historyEntityPage = Links.query({ pageNumber: $scope.pageNumber, pageSize: $scope.pageSize, userId: $rootScope.UserId }, function () {
+        $scope.search = function() {
+            $scope.historyEntityPage = Links.query({ pageNumber: $scope.pageNumber, pageSize: $scope.pageSize, userId: $rootScope.UserId }, function() {
                 $scope.history = $scope.historyEntityPage.List;
             });
-        }
+        };
 
-        $scope.reset = function () {
+        $scope.reset = function() {
             $scope.pageNumber = 0;
             $scope.pageSize = 10;
             $scope.search();
-        }
+        };
 
         $scope.onShortLinkClick = function(item) {
             item.Visits++;
-        }
+        };
+
+        $scope.onPrevClick = function () {
+            if ($scope.pageNumber > 0) {
+                $scope.pageNumber--;
+                $scope.search();
+            }
+
+        };
+
+        $scope.onNextClick = function () {
+            if ($scope.pageNumber + 1 < $scope.historyEntityPage.PageCount) {
+                $scope.pageNumber++;
+                $scope.search();
+            }
+        };
+
 
         $scope.reset();
 
