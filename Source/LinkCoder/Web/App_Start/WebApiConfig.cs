be9c1ba6674388cfaa3bb34fd8084e5fc411fff4
IlyaOvcharenko <ibovcharenko@gmail.com>
2017-08-21 02:13:03 +0300

    Cors enabled

--- Source/LinkCoder/Web/App_Start/WebApiConfig.cs
+++ Source/LinkCoder/Web/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using Web.Filters;
 
 namespace Web
@@ -11,7 +12,8 @@ namespace Web
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
             // Web API routes
             config.MapHttpAttributeRoutes();
 
